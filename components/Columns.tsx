8cc2caf1ab10713f543fe5169ddf9f523f5ccf01
Hardik Verma <hardikverma22@rediffmail.com>
2023-12-11 19:54:14 +0530

    build: fixing build issue removing extra min prop

--- components/Columns.tsx
+++ components/Columns.tsx
@@ -9,7 +9,7 @@ const Columns = () => {
     <div className="w-full h-max">
       <section className="flex gap-6 h-max">
         {columns.map((col) => (
-          <Column columnTitle={col.name} status={col.id} key={col.id} max={col.max} min={col.min} />
+          <Column columnTitle={col.name} status={col.id} key={col.id} max={col.max} />
         ))}
         {/* <Column columnTitle="To-Do" status="TODO" />
         <Column columnTitle="In-Progress" status="IN_PROGRESS" />
